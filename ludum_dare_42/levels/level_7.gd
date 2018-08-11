9d9b2007078959567d9047bfa4f4376ef80357f4
Paulb23 <p_batty@hotmail.co.uk>
2018-08-11 21:36:19 +0100

    adjusted level 7

--- ludum_dare_42/levels/level_7.gd
+++ ludum_dare_42/levels/level_7.gd
@@ -1,18 +1,20 @@
 extends TileMap
 
 var switched_tiles = {
-	Vector2(11, -2):
+	Vector2(14, -1):
 	[
 		{
-			Vector2(10,0): 0,
-			Vector2(10,-1): 0
+			Vector2(13,0): 0,
+			Vector2(13,1): 0
 		}
 	]
 }
 
 func switch_pressed(tile: Vector2) -> void:
+	print(tile)
 	if (not switched_tiles.has(tile)):
 		return
+
 	for new_tiles in switched_tiles[tile]:
 		for cell in new_tiles:
 			set_cellv(cell, new_tiles[cell])
