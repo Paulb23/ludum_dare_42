7d0e998d9efdbab3d59b345eb007c0ebc2374b9c
Paulb23 <p_batty@hotmail.co.uk>
2018-08-11 10:44:15 +0100

    falling

--- ludum_dare_42/game_manager.gd
+++ ludum_dare_42/game_manager.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 func _ready() -> void:
+	_reset()
+	$player.connect("fallen", self, "_reset")
+
+func _reset() -> void:
 	var start_id : Array = $TileMap.get_used_cells_by_id(1)
 	assert(start_id.size() > 0)
 	$player.set_start_tile(start_id.pop_back())
--- ludum_dare_42/player/player.gd
+++ ludum_dare_42/player/player.gd
@@ -8,21 +8,51 @@ enum Angle {
 	VERTICAL
 }
 
-var start = false
+signal fallen
+
+var falling : bool = false
+var moving : bool = false
+var can_move : bool = false
 
 var current_tile : Vector2
 var start_tile : Vector2
 var current_angle : int = VERTICAL
 
 func _ready() -> void:
-	pass
+	$move_delay.connect("timeout", self, "allow_movement")
 
 func _physics_process(delta : float) -> void:
-	if (not start):
+
+	if (can_move and not falling):
+		if (get_tile_id(current_tile) == -1):
+			falling = true
+
+		match current_angle:
+			VERTICAL:
+				pass
+			RIGHT:
+				if (get_tile_id(Vector2(current_tile.x, current_tile.y - 1)) == -1):
+					falling = true
+			LEFT:
+				if (get_tile_id(Vector2(current_tile.x, current_tile.y + 1)) == -1):
+					falling = true
+			UP:
+				if (get_tile_id(Vector2(current_tile.x - 1, current_tile.y)) == -1):
+					falling = true
+			DOWN:
+				if (get_tile_id(Vector2(current_tile.x + 1, current_tile.y)) == -1):
+					falling = true
+		if (falling):
+			emit_signal("fallen")
+			return
+
+	if (falling or not can_move):
 		return
 
-	if (Input.is_action_just_pressed("ui_up")):
+	var moved : bool = false
+	if (Input.is_action_pressed("ui_up")):
 		current_tile.x -= 1
+		moved = true
 		match current_angle:
 			VERTICAL:
 				$AnimationPlayer.play("up")
@@ -38,9 +68,9 @@ func _physics_process(delta : float) -> void:
 			DOWN:
 				$AnimationPlayer.play("vertical")
 				current_angle = VERTICAL
-
-	if (Input.is_action_just_pressed("ui_down")):
+	elif (Input.is_action_pressed("ui_down")):
 		current_tile.x += 1
+		moved = true
 		match current_angle:
 			VERTICAL:
 				$AnimationPlayer.play("down")
@@ -56,9 +86,9 @@ func _physics_process(delta : float) -> void:
 				current_tile.x += 1
 				$AnimationPlayer.play("vertical")
 				current_angle = VERTICAL
-
-	if (Input.is_action_just_pressed("ui_left")):
+	elif (Input.is_action_pressed("ui_left")):
 		current_tile.y += 1
+		moved = true
 		match current_angle:
 			VERTICAL:
 				$AnimationPlayer.play("left")
@@ -74,9 +104,9 @@ func _physics_process(delta : float) -> void:
 				pass
 			DOWN:
 				pass
-
-	if (Input.is_action_just_pressed("ui_right")):
+	elif (Input.is_action_pressed("ui_right")):
 		current_tile.y -= 1
+		moved = true
 		match current_angle:
 			VERTICAL:
 				$AnimationPlayer.play("right")
@@ -93,19 +123,38 @@ func _physics_process(delta : float) -> void:
 			DOWN:
 				pass
 
-	position = convert_map_to_world(current_tile)
-	position.x -= 0
-	position.y += 10
+	if (moved):
+		position = convert_map_to_world(current_tile)
+		position.x -= 0
+		position.y += 10
+
+		disllow_movement()
+		$move_delay.start()
 
 func set_start_tile(start_tile : Vector2) -> void:
 	self.start_tile = start_tile
 	self.current_tile = start_tile
-	start = true
 	position = convert_world_to_map(start_tile)
+	position = convert_map_to_world(current_tile)
+	position.x -= 0
+	position.y += 10
+	$AnimationPlayer.play("vertical")
 	current_angle = VERTICAL
+	falling = false
+	disllow_movement()
+	$move_delay.start()
+
+func allow_movement() -> void:
+	can_move = true
+
+func disllow_movement() -> void:
+	can_move = false
 
 func convert_world_to_map(coordinates : Vector2) -> Vector2:
 	return get_parent().get_node("TileMap").world_to_map(coordinates)
 
 func convert_map_to_world(coordinates : Vector2) -> Vector2:
 	return get_parent().get_node("TileMap").map_to_world(coordinates)
+
+func get_tile_id(coordinates : Vector2) -> int:
+	return 	get_parent().get_node("TileMap").get_cellv(coordinates)
