f7e1844045bec650e8a1d5ff6eb49dbc97fc177d
Paulb23 <p_batty@hotmail.co.uk>
2018-08-11 23:18:47 +0100

    vol slider now work

--- ludum_dare_42/menus/main_menu.gd
+++ ludum_dare_42/menus/main_menu.gd
@@ -3,6 +3,17 @@ extends Control
 func _ready() -> void:
 	$exit.connect("pressed", self, "_exit")
 	$play.connect("pressed", self, "_play")
+	$music_vol.connect("value_changed", self, "_music_vol_changed")
+	$music_vol.value = AudioServer.get_bus_volume_db(abs(AudioServer.get_bus_index("Music")))
+
+	$sfx_vol.connect("value_changed", self, "_sfx_vol_changed")
+	$sfx_vol.value = AudioServer.get_bus_volume_db(abs(AudioServer.get_bus_index("SFX")))
+
+func _music_vol_changed(val):
+	AudioServer.set_bus_volume_db(abs(AudioServer.get_bus_index("Music")), val)
+
+func _sfx_vol_changed(val):
+	AudioServer.set_bus_volume_db(abs(AudioServer.get_bus_index("SFX")), val)
 
 
 func _play():
