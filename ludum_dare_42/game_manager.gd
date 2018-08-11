91849ec3d0d52707582c16b8067efb14dfb16303
Paulb23 <p_batty@hotmail.co.uk>
2018-08-11 17:10:24 +0100

    more levels

--- ludum_dare_42/game_manager.gd
+++ ludum_dare_42/game_manager.gd
@@ -6,7 +6,9 @@ var levels = [
 	"res://levels/level_1.tscn",
 	"res://levels/level_2.tscn",
 	"res://levels/level_3.tscn",
-	"res://levels/level_4.tscn"
+	"res://levels/level_4.tscn",
+	"res://levels/level_5.tscn",
+	"res://levels/level_6.tscn"
 ]
 
 func _ready() -> void:
